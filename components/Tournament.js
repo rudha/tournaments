073bec7cc8ee48df571a2e73c7614b68f0d5d759
Rudhá Oliveira <rudha94@gmail.com>
2018-04-24 20:07:06 -0300

    Added buttons.

--- components/Tournament.js
+++ components/Tournament.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View, StyleSheet, ViewPagerAndroid } from 'react-native';
+import { Text, View, StyleSheet, Button, ViewPagerAndroid } from 'react-native';
 
 export default class Tournament extends React.Component {
 	state = {
-		player1: "",
-		player2: "",
-		player3: "",
-		player4: "",
+		winner1: "",
+		winner2: "",
+		loser1: "",
+		loser2: "",
 	}
 
 	static navigationOptions = {
@@ -15,6 +15,10 @@ export default class Tournament extends React.Component {
 			fontWeight: 'normal',
 		},
 	};
+
+	winnerHandler = (player) => {
+		
+	}
 
 	render() {
 		const { params } = this.props.navigation.state;
@@ -28,8 +32,16 @@ export default class Tournament extends React.Component {
 					<Text>Player 4: {params.tournament.player4}</Text>
 				</View>
 				<View style={styles.pageStyle} key="2">
-					<Text>{params.tournament.player1} vs {params.tournament.player2}</Text>
-					<Text>{params.tournament.player3} vs {params.tournament.player4}</Text>
+					<View style={styles.matchRow}>
+						<Button title="WINNER" onPress={() => this.winnerHandler(params.tournament.player1)} />
+						<Text>{params.tournament.player1} vs {params.tournament.player2}</Text>
+						<Button title="WINNER" />
+					</View>
+					<View style={styles.matchRow}>
+						<Button title="WINNER" />
+						<Text>{params.tournament.player3} vs {params.tournament.player4}</Text>
+						<Button title="WINNER" />
+					</View>
 				</View>
 			</ViewPagerAndroid>
 		);
@@ -44,4 +56,10 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		padding: 10,
 	},
+	matchRow: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+	}
 });
